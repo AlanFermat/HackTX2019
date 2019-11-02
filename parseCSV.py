1b5b2e5f957ee73d95c4619b00a94e0593967560
AlanFermat <zy19@rice.edu>
2019-11-02 13:51:50 -0500

    sunsung csv done

--- parseCSV.py
+++ parseCSV.py
@@ -3,27 +3,22 @@ import csv
 def parse(filename, headerline, footline):
 	result = []
 	write_to = open("new" + filename, 'w')
+	company_name = None
 	with open(filename) as csv_file:
 		line_count = 0
 		for line in csv_file:
 			if line_count <= headerline or line_count >= footline:
 				line_count += 1
 				continue
-			if line[-2] == ",":
-				result.append(line)
+			row = []
+			if line[0] != ",":
+				row = line.split(",")
+				company_name = row[0]
+				# print ("".join(row[1:]))
+				write_to.write(company_name + "," + "".join(row[1:]))
 			else:
-				if result[-1][-2]==',':
-					result[-1] += line
-				else:
-					result.append(line)
-			line_count += 1
-	for company in result:
-		row = company.split("\t")
-		print(row)
-		# if row[1]:
-		# 	address = row[1].split("\n")
-		# 	print(address)
+				write_to.write(company_name + line)
 	return result
 
 
-parse('Sumsung-Supplier-List.csv', 1, 1450)
+parse('Sumsung-Supplier-List.csv', -1, 1450)
