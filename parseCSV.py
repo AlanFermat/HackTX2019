c963328452a61ba2c88b6db91dae129f739fd423
AlanFermat <zy19@rice.edu>
2019-11-02 15:04:54 -0500

    acer done

--- parseCSV.py
+++ parseCSV.py
@@ -7,23 +7,9 @@ def parse(filename, headerline, footline):
 	with open(filename) as csv_file:
 		line_count = 0
 		for line in csv_file:
-			if line_count <= headerline or line_count >= footline:
-				line_count += 1
-				continue
-			if line[0] == ",":
-				line = line[1:]
-			if line[:4] != "Page":
-				line_split = line.split(",,")
-				if len(line_split) == 2:
-					write_to.write(line_split[0] + "," + line_split[1])
-				elif len(line_split) == 1:
-					line = line_split[0]
-					if line[-2:] == ",\n":
-						write_to.write(line[:-2] + '\n')
-					else:
-						write_to.write(line)
-			line_count += 1
+			row = line.split(" \t")
+			write_to.write(row[0] + "," + "\"" + row[1][:-1] + "\"" + "\n")
 	return result
 
 
-parse('lenovo-supplier-list.csv', 16, 80)
+parse('acer-supplier-list.csv', 0,0)
