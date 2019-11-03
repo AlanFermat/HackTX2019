3879c40ae693246b9b887ac8418eef3734ce19dd
AlanFermat <zy19@rice.edu>
2019-11-03 01:53:16 -0600

    refactor

--- smart-investment/src/component/companyInfo.jsx
+++ smart-investment/src/component/companyInfo.jsx
@@ -4,13 +4,12 @@ import React, {
 import { Neo4jGraphRenderer } from 'neo4j-graph-renderer';
   class CompanyInfo extends Component {
     render() {
-    return (
-      <div>
-      <Neo4jGraphRenderer url={process.env.REACT_APP_NEO4J_URI} user={process.env.REACT_APP_NEO4J_USER}
+      let visual = <Neo4jGraphRenderer url={process.env.REACT_APP_NEO4J_URI} user={process.env.REACT_APP_NEO4J_USER}
         password={process.env.REACT_APP_NEO4J_PASSWORD} 
-        query="match (c1:Company)<-[r1:IS_SUPPLIER_FOR]-(n:Supplier)-[r2:IS_SUPPLIER_FOR]->(c2:Company) return n,r1,c1,r2,c2"/>
-    </div>
-    );
+        query="match (c1:Company)<-[r1:IS_SUPPLIER_FOR]-(n:Supplier)-[r2:IS_SUPPLIER_FOR]->(c2:Company) return n,r1,c1,r2,c2"/>; 
+      return (
+        visual
+      );
   } 
 }  
 
