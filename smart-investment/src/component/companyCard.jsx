947ef1781b9c421c10f06f98907de427099a2456
AlanFermat <zy19@rice.edu>
2019-11-03 03:32:34 -0600

    fix alignment and margins

--- smart-investment/src/component/companyCard.jsx
+++ smart-investment/src/component/companyCard.jsx
@@ -39,7 +39,7 @@ class CompanyCard extends Component {
 	    }
   	}
 	render() {
-		const Query = "match (c:Company)<-[r:IS_SUPPLIER_FOR]-(n:Supplier) where c.name = '" + this.props.name + "' return n,r,c limit 50";
+		const Query = "match (c:Company)<-[r:IS_SUPPLIER_FOR]-(n:Supplier) where c.name = '" + this.props.name + "' return n,r,c limit 40";
 		let description;
 		if (this.state.score >= 80) {
 			description = this.props.name+" Inc. is a company that shows a high score on the overall evaluation of " + 
@@ -57,34 +57,33 @@ class CompanyCard extends Component {
 		return (
 			<div>
 			<Row md="6">
-				<Col sm="8" className="text-center"> 
+				<Col sm="7" className="text-center"> 
 					<h1>{this.props.name}</h1>
 				</Col>
-				<Col md="4" className="text-center">
+				<Col md="5" className="text-center">
 					<h1>Score {this.state.score}</h1>
 				</Col>
 			</Row>
 			<Row md="6"> 
-				<Col md="8"> 
+				<Col md="7"> 
 					<Row md="2">
-						<Card body className="text-left" style={{backgroundColor: '#f5f5f5'}}> 
+						<Card body className="text-left" style={{backgroundColor: '#f5f5f5', paddingLeft: "3rem"}}> 
 							<CardText> 
 								{description}
 							</CardText>									
 						</Card>
 					</Row>
 					<Row md="6">
-						<Card body style={{height: '49rem', backgroundColor: '#f5f5f5' }}>
+						<Card body style={{height: '60rem', backgroundColor: '#f5f5f5', paddingLeft:"3rem" }}>
 							<Neo4jGraphRenderer url={process.env.REACT_APP_NEO4J_URI} user={process.env.REACT_APP_NEO4J_USER}
 			        password={process.env.REACT_APP_NEO4J_PASSWORD} 
 			        query={Query}/>
 						</Card>
 					</Row>
-
 				</Col>
-				<Col md="4"> 
+				<Col md="5"> 
 					<Row md="12">
-						<Card body style={{ marginTop: '0%', height: '24rem',backgroundColor: '#f5f5f5' }}>
+						<Card body style={{ marginTop: '0%', height: '14rem',backgroundColor: '#f5f5f5',paddingRight:"3rem" }}>
 				            <Table striped bordered hover size="sm">
 							  <thead>
 							    <tr>
@@ -117,9 +116,17 @@ class CompanyCard extends Component {
 				        </Card>
 					</Row>
 					<Row md="12">
-						<Card body style={{height: '30rem', marginBottom:'0%', backgroundColor: '#f5f5f5'}}>
+						<Card body style={{height: '26rem', backgroundColor: '#f5f5f5'}}>
 				          <MDBContainer>
-					        <h3 className="mt-5">Pie chart</h3>
+					        <h3 className="mt-2">Pie chart</h3>
+					        <Pie data={this.state.dataPie} options={{ responsive: true }} />
+					      </MDBContainer>
+				        </Card>
+					</Row>
+					<Row md="12">
+						<Card body style={{height: '26rem', backgroundColor: '#f5f5f5'}}>
+				          <MDBContainer>
+					        <h3 className="mt-2">Pie chart</h3>
 					        <Pie data={this.state.dataPie} options={{ responsive: true }} />
 					      </MDBContainer>
 				        </Card>
